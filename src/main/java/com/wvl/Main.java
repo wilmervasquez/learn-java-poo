6cac71e1a74dacd99dbdcac29373d0c4f5baa512
Wilmer Vasquez <wilmerluisvasquezlino@gmail.com>
2025-09-17 11:32:29 -0500

    v0.0.2

--- src/main/java/com/wvl/Main.java
+++ src/main/java/com/wvl/Main.java
@@ -1,7 +1,36 @@
 package com.wvl;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+class Book {
+  String id;
+  String titulo;
+  String autor;
+  String editora;
+
+  Book(String titulo, String autor, String editora) {
+    this.id = UUID.randomUUID().toString();
+    this.titulo = titulo;
+    this.autor = autor;
+    this.editora = editora;
+  }
+}
+
+class Library {
+  List<Book> books;
+  Library() {
+    this.books = new ArrayList<>();
+  }
+  void addBook(Book book) {
+    this.books.add(book);
+  }
+  void deleteBook(Book book) {
+    books.remove(book);
+  }
+}
 public class Main {
   public static void main(String[] args) {
     System.out.println(Math.sin(Math.toRadians(60)));
@@ -11,5 +40,9 @@ public class Main {
     JOptionPane.showMessageDialog(null,"222");
 
     System.out.println(bug);
+
+    Library library = new Library();
+
+    library.addBook(new Book("Principito", "Diego", "IBMD"));
   }
 }
